cfaa144f603ed9df52ff47715368d30fbf2f638d
Chase Moskal <chasemoskal@gmail.com>
2023-07-18 01:15:58 -0700

    add: flatstate and pipe to index exports

--- s/index.ts
+++ s/index.ts
@@ -1,5 +1,6 @@
 
 export * from "./base/element.js"
+export * from "./base/helpers/mix_flatstate_reactivity_into_elements.js"
 export * from "./base/helpers/pass_context_to_elements.js"
 export * from "./base/helpers/register_elements.js"
 export * from "./base/helpers/theme_elements.js"
@@ -7,6 +8,7 @@ export * from "./base/helpers/update_elements_on_cue_changes.js"
 export * from "./base/helpers/update_elements_on_snap_changes.js"
 export * from "./base/mixins/css.js"
 export * from "./base/mixins/cues.js"
+export * from "./base/mixins/flatstate.js"
 export * from "./base/mixins/snap.js"
 export * from "./base/utils/finalize_styles.js"
 
@@ -15,8 +17,12 @@ export * from "./cues/group.js"
 export * from "./cues/parts/listener.js"
 export * from "./cues/parts/circular_error.js"
 
+export * from "./flatstate/flatstate.js"
+
 export * from "./quick/element.js"
 
-export * from "./tools/pub.js"
 export * from "./tools/explode_promise.js"
+export * from "./tools/maptool.js"
+export * from "./tools/pipe.js"
+export * from "./tools/pub.js"
 
