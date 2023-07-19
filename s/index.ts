43cc049a6c614005be42f9139fe598b54c52d23a
Chase Moskal <chasemoskal@gmail.com>
2023-07-19 14:06:01 -0700

    export: flatstate errors

--- s/index.ts
+++ s/index.ts
@@ -18,6 +18,7 @@ export * from "./cues/parts/listener.js"
 export * from "./cues/parts/circular_error.js"
 
 export * from "./flatstate/flatstate.js"
+export * from "./flatstate/parts/errors.js"
 
 export * from "./quick/element.js"
 
