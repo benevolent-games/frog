21ba7e03032d2d76cb647510a784d5fcf54ae21c
Chase Moskal <chasemoskal@gmail.com>
2023-07-20 21:21:37 -0700

    rename: flat3 to flatstate

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,10 +1,8 @@
 
 import {Suite} from "cynic"
-// import flatstate from "./flatstate/flatstate.test.js"
-import flat3 from "./flatstate/flat3.test.js"
+import flatstate from "./flatstate/flatstate.test.js"
 
 export default <Suite>{
-	// flatstate
-	flat3,
+	flatstate,
 }
 
